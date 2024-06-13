ac42c0187c2197e5f0c851ccaeb862b0ea16bb3c
Johnny Walker <748017+johnnywalker@users.noreply.github.com>
2024-06-13 09:20:12 -0400

    fix: configure guake on nixos

---
+++ home/johnny/common/nixos/guake.nix
@@ -0,0 +1,13 @@
+{...}: {
+  dconf.settings = {
+    "org/guake/keybindings/global" = {
+      show-hide = "<Primary>grave";
+    };
+    "org/guake/style/background" = {
+      transparency = 90;
+    };
+    "org/guake/style/font" = {
+      style = "JetBrainsMono Nerd Font Mono 11";
+    };
+  };
+}
