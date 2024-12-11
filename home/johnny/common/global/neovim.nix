10085a2588f537ce5acef17ef060225d513892a2
Johnny Walker <jwalker@designsforhealth.com>
2024-12-11 11:01:57 -0500

    fix: disable many neovim plugins for now

--- home/johnny/common/global/neovim.nix
+++ home/johnny/common/global/neovim.nix
@@ -31,99 +31,99 @@
       '';
       plugins = with pkgs.vimPlugins; [
         # dependencies
-        plenary-nvim
-        nui-nvim
-        nvim-web-devicons
+        # plenary-nvim
+        # nui-nvim
+        # nvim-web-devicons
         # treesitter
         nvim-treesitter.withAllGrammars
-        nvim-treesitter-context
-        nvim-treesitter-textobjects
+        # nvim-treesitter-context
+        # nvim-treesitter-textobjects
         # completion
-        {
-          plugin = nvim-cmp;
-          optional = false;
-        }
-        {
-          plugin = cmp-buffer;
-          optional = false;
-        }
-        {
-          plugin = cmp-nvim-lsp;
-          optional = false;
-        }
-        {
-          plugin = cmp-path;
-          optional = false;
-        }
-        {
-          plugin = cmp_luasnip;
-          optional = false;
-        }
+        # {
+        #   plugin = nvim-cmp;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = cmp-buffer;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = cmp-nvim-lsp;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = cmp-path;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = cmp_luasnip;
+        #   optional = false;
+        # }
         # lsp
-        {
-          plugin = nvim-lspconfig;
-          optional = false;
-        }
-        {
-          plugin = neodev-nvim;
-          optional = false;
-        }
-        {
-          plugin = null-ls-nvim;
-          optional = false;
-        }
-        {
-          plugin = luasnip;
-          optional = false;
-        }
-        {
-          plugin = friendly-snippets;
-          optional = false;
-        }
-        {
-          plugin = fidget-nvim;
-          optional = false;
-        }
+        # {
+        #   plugin = nvim-lspconfig;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = neodev-nvim;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = null-ls-nvim;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = luasnip;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = friendly-snippets;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = fidget-nvim;
+        #   optional = false;
+        # }
         # dap
-        {
-          plugin = nvim-dap;
-          optional = true;
-        }
-        {
-          plugin = nvim-dap-ui;
-          optional = true;
-        }
-        {
-          plugin = nvim-dap-virtual-text;
-          optional = true;
-        }
+        # {
+        #   plugin = nvim-dap;
+        #   optional = true;
+        # }
+        # {
+        #   plugin = nvim-dap-ui;
+        #   optional = true;
+        # }
+        # {
+        #   plugin = nvim-dap-virtual-text;
+        #   optional = true;
+        # }
         # telescope
-        {
-          plugin = telescope-nvim;
-          optional = false;
-        }
-        {
-          plugin = telescope-fzf-native-nvim;
-          optional = false;
-        }
+        # {
+        #   plugin = telescope-nvim;
+        #   optional = false;
+        # }
+        # {
+        #   plugin = telescope-fzf-native-nvim;
+        #   optional = false;
+        # }
         # statusline
-        lualine-nvim
-        nvim-navic
+        # lualine-nvim
+        # nvim-navic
         # misc
-        boole-nvim
-        comment-nvim
-        {
-          plugin = diffview-nvim;
-          optional = false;
-        }
+        # boole-nvim
+        # comment-nvim
+        # {
+        #   plugin = diffview-nvim;
+        #   optional = false;
+        # }
         gitsigns-nvim
-        {
-          plugin = harpoon;
-          optional = false;
-        }
-        impatient-nvim
-        indent-blankline-nvim
-        vim-sleuth
+        # {
+        #   plugin = harpoon;
+        #   optional = false;
+        # }
+        # impatient-nvim
+        # indent-blankline-nvim
+        # vim-sleuth
         which-key-nvim
         # ui
         {
