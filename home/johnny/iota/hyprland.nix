a80b5e60fef69ba143058532fd355545acba696c
Johnny Walker <jwalker@designsforhealth.com>
2025-01-07 17:04:56 -0500

    feat: configure wallpaper

--- home/johnny/iota/hyprland.nix
+++ home/johnny/iota/hyprland.nix
@@ -331,6 +331,16 @@ in {
   };
   systemd.user.services.hypridle.Unit.After = ["graphical-session.target"];
 
+  services.hyprpaper.enable = true;
+  services.hyprpaper.settings = let
+    wallpaper = ./wallpapers/53852285870_01497c017d_o.png;
+  in {
+    ipc = "on";
+    preload = ["${wallpaper}"];
+    wallpaper = [", ${wallpaper}"];
+  };
+  systemd.user.services.hyprpaper.Unit.After = ["graphical-session.target"];
+
   gtk = {
     enable = true;
     iconTheme = {
