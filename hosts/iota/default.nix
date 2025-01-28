322bd99b72edcde6a3f4f2083e5617f570fe1bf7
Johnny Walker <jwalker@designsforhealth.com>
2025-01-28 16:11:43 -0500

    feat: enable cpupower-gui

--- hosts/iota/default.nix
+++ hosts/iota/default.nix
@@ -80,6 +80,8 @@
   };
   programs.noisetorch.enable = true;
 
+  services.cpupower-gui.enable = true;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   # environment.systemPackages = [];
