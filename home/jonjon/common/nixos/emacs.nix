4494a9888ea56c2ef0934432a072349f6e536721
Johnny Walker <jwalker@designsforhealth.com>
2025-04-04 12:48:52 -0400

    fix: update emacs29-pgtk to emacs30-pgtk

--- home/jonjon/common/nixos/emacs.nix
+++ home/jonjon/common/nixos/emacs.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs29-pgtk;
+    package = pkgs.emacs30-pgtk;
     extraPackages = epkgs: [
       # (epkgs.treesit-grammars.with-grammars (grammars: with grammars; [tree-sitter-bash tree-sitter-dockerfile]))
       epkgs.treesit-grammars.with-all-grammars
