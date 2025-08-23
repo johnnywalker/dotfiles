e8c502d91aeb5e24d37ffa37c22a3a2a19b718ea
Johnny Walker <jwalker@designsforhealth.com>
2025-08-23 15:55:39 -0400

    fix: add desktop entry for emacs with vanilla config

--- home/johnny/common/nixos/emacs.nix
+++ home/johnny/common/nixos/emacs.nix
@@ -27,4 +27,19 @@
   # services.emacs.enable = true;
   # services.emacs.socketActivation.enable = true;
   # services.emacs.startWithUserSession = true;
+  xdg.desktopEntries.emacs-vanilla = {
+    name = "Emacs (Vanilla)";
+    genericName = "Text Editor";
+    exec = ''
+      emacs --init-directory /home/johnny/.emacs.vanilla.d %F
+    '';
+    terminal = false;
+    icon = "emacs";
+    categories = ["Development" "TextEditor"];
+    type = "Application";
+    settings = {
+      StartupNotify = "true";
+      StartupWMClass = "Emacs";
+    };
+  };
 }
