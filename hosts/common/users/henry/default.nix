651bf2d0be2bb43b0ceddc138f5adffee1bc0f55
Johnny Walker <jwalker@designsforhealth.com>
2025-10-23 13:24:59 -0400

    fix: configure authorized keys for henry

--- hosts/common/users/henry/default.nix
+++ hosts/common/users/henry/default.nix
@@ -11,6 +11,12 @@
         isNormalUser = true;
         hashedPasswordFile = config.sops.secrets.henry-password.path;
         shell = pkgs.zsh;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK52M5lAGnnRDpjYnPPgZX9Lz5SEfvARj23ecUPSvBHX"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhL2s7nRiFBw8U0SMQPWCsaWQXc51YMP8ga81Uqm9Rx"
+          # johnny@petey
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHudwtU+wqmxkbhlsg8/Lv8FII0Vu68xf9xHVxojkStM"
+        ];
       };
     };
   };
